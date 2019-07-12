868ac73cfa73640a322007b55a3090fc0d15173a
shiyuetong <52817564+shiyuetong@users.noreply.github.com>
2019-07-12 16:16:35 +0800

    Add files via upload

---
+++ syt-7-12.cpp
@@ -0,0 +1,87 @@
+
+
+#include<iostream>//有#在c++中，是预处理命令
+// #include<algorithm>
+// #include<string>
+
+using namespace std;//命名空间
+
+//每一行结束，必须要有分号
+
+int main(){//必须要有的
+
+    int a,b;
+
+    cin>>a>>b;//输入语句
+    // >>输入运算符
+    if(a>b){
+        cout<<"the max number is:"<<a<<endl;
+    }
+    else{//无小括号
+        cout<<"the max number is:"<<b<<endl;
+    }
+    // cout<<a+b<<endl;//输出
+    // <<输出运算符
+    return 0;//直接写就行
+}
+
+
+// 选择语句：
+// ==============
+// if(){
+
+// }
+// ===============
+// if(){
+
+// }else if(){
+
+// }else if(){
+
+// }else if(){
+
+// }
+// ==============
+
+// if(){
+
+// }else{//else没有小括号
+
+// }
+// =============
+// if(){
+
+// }else if(){
+
+// }else{//无小括号
+
+// }
+// // ===========
+// if(){
+
+// }else if(){
+
+// }else if(){
+
+// }else if(){
+
+// }else{
+
+// }
+// ==============
+// if(){
+//     if(){
+
+//     }else{
+
+//     }
+// }else if{
+
+// }
+// ==============
+
+
+// =赋值符号
+// ==等于运算符
+
+// &&  ||  !=
